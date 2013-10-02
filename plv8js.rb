efeb1b9b51051a20e82512d7d582c05135e0f252
Timothy Klim <klimtimothy@gmail.com>
2013-10-02 17:26:46 +0400

    Update plv8js.rb

--- plv8js.rb
+++ plv8js.rb
@@ -3,17 +3,24 @@ require 'formula'
 class Plv8js < Formula
   homepage 'https://code.google.com/p/plv8js'
 
-  url 'https://plv8js.googlecode.com/files/plv8js-20120719.tar.gz'
-  sha1 '923ca6ce77e698eeea0aae6dc4cb33d6f70dc094'
-  version '1.1.0'
+  url 'https://plv8js.googlecode.com/files/plv8-1.4.1.zip'
+  sha1 'ceb7579b1fae1c1fe795c03a23471fdf9c7c469e'
+  version '1.4.1'
 
   head 'https://code.google.com/p/plv8js.git'
 
-  depends_on 'v8'
-  depends_on 'postgresql'
+  depends_on :v8
+  depends_on :postgresql
 
   def install
+    ENV.prepend 'PATH', Formula.factory('postgresql').bin, ':'
+    system 'pg_config'
     system "make"
-    system "make", "PREFIX=#{prefix}", "install"
+    system "make install"
+  end
+ 
+  def test
+    ENV.prepend 'PATH', Formula.factory('postgresql').bin, ':'
+    system "make installcheck"
   end
 end
