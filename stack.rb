d6b90c7a3a21b4a242ec0c761eaf38493e7adc93
Timothy Klim <klimtimothy@gmail.com>
2015-07-01 19:48:52 +0300

    chore: bump stack version

--- stack.rb
+++ stack.rb
@@ -1,9 +1,9 @@
 class Stack < Formula
   desc "The Haskell Tool Stack"
   homepage "https://github.com/commercialhaskell/stack"
-  url "https://github.com/commercialhaskell/stack/releases/download/v0.1.0.0/stack-0.1.0.0-x86_64-osx.gz"
-  version "0.1.0.0"
-  sha256 "0250d3fcb87d48829ddfb999ee8d71153ee1c81087af07f8e3a506450dc41225"
+  url "https://github.com/commercialhaskell/stack/releases/download/v0.1.1.0/stack-0.1.1.0-x86_64-osx.gz"
+  version "0.1.1.0"
+  sha256 "81f86fe0e7c72bd3227247b958151f0036c06fdd3ccc50b38ea0ea443a441475"
 
   def install
     bin.install "stack-#{version}-x86_64-osx" => "stack"
