9e1ec23fc76dd1756c06718dfaf57e623a7dd41a
Timothy Klim <klimtimothy@gmail.com>
2015-08-22 15:48:36 +0300

    chore: bump stack to version 0.1.3.1

--- stack.rb
+++ stack.rb
@@ -1,8 +1,8 @@
 class Stack < Formula
   desc "The Haskell Tool Stack"
   homepage "https://github.com/commercialhaskell/stack"
-  url "https://github.com/commercialhaskell/stack/releases/download/v0.1.2.0/stack-0.1.2.0-x86_64-osx.gz"
-  version "0.1.2.0"
+  url "https://github.com/commercialhaskell/stack/releases/download/v0.1.3.1/stack-0.1.3.1-x86_64-osx.gz"
+  version "0.1.3.1"
   sha256 "6e1039d9c5144fb03dbfb1f569a830724593191305998e9be87579d985feb36c"
 
   def install
